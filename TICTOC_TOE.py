a2439f9f871a4d32e124b71533050f6bfaeb98ab
PetrusAndreyProton <Petrus_Andrii@protonmail.com>
2023-09-24 22:12:14 +0300

    Добавил функцию inputPlayerLetter, игрок вводит букву которую он выбрал.

--- TICTOC_TOE.py
+++ TICTOC_TOE.py
@@ -1,8 +1,21 @@
 import random
 
+
 def drawBoard(board):
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-+-+-')
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
     print(board[1] + '|' + board[2] + '|' + board[3])
+
+
+def inputPlayerLatter():
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        print('Вы выбираете Х или О')
+        letter = input().upper()
+
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
